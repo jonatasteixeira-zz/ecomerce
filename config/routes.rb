2a0e6c7633bd94266855c8c6066535a3b00cbef8
Jonatas Teixeira <jonatas.teixeira@itau-unibanco.com.br>
2016-12-07 21:22:11 -0200

    Adicionando carrinho

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :carts
   resources :items
   
   get     '/logout'                         => 'sessions#destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207222650) do
+ActiveRecord::Schema.define(version: 20161207231656) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "carts", ["user_id"], name: "index_carts_on_user_id"
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +29,14 @@ ActiveRecord::Schema.define(version: 20161207222650) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "items_cart", force: :cascade do |t|
+    t.integer "items_id"
+    t.integer "cart_id"
+  end
+
+  add_index "items_cart", ["cart_id"], name: "index_items_cart_on_cart_id"
+  add_index "items_cart", ["items_id"], name: "index_items_cart_on_items_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
